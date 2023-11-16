cdf7ea10a57db39f0dc6e154fb4ddbd89decea6b
ddefreitas.exe <darrell.defreitas@gmail.com>
2023-11-16 12:37:18 -0500

    Testing push to file

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import shutil
 
 print("start of processing")
 shutil.copytree(sys.argv[1], sys.argv[2], dirs_exist_ok=True)
+# Push changes here to repo
+print("Hello World from Nov Hackathon")
 print("end of processing")
